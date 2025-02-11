c51bb7f845d766f2b0589166ce8a6dabf0a1e75d
sktrich <r.arumakan@gmail.com>
2025-02-11 12:52:48 -0300

    new features

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import Home from "./pages/Home";
 import Footer from "./pages/Footer";
+import Bloob from "./pages/BG";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Footer />
+    <Bloob/>
     <Home />
     <Footer />
   </StrictMode>
---
+++ src/pages/BG/index.jsx
@@ -0,0 +1,17 @@
+import "./styles.css";
+
+function Bloob(){
+    return(
+        <>
+            <div class="blob-outer-container">
+                <div class="blob-inner-container">
+                    <div class="blob"></div>
+                </div>
+            </div>
+        
+        </>
+    )
+
+}
+
+export default Bloob
